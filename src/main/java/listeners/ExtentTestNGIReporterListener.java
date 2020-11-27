efcdd5c603cd9eb51a962a2ab1768dd3f3d7240d
zhangjingli <zhangjingli@baiwang.com>
2020-11-27 16:09:34 +0800

    修改报告名称

--- src/main/java/listeners/ExtentTestNGIReporterListener.java
+++ src/main/java/listeners/ExtentTestNGIReporterListener.java
@@ -20,7 +20,7 @@ import java.util.*;
 public class ExtentTestNGIReporterListener implements IReporter {
     //生成的路径以及文件名
     private static final String OUTPUT_FOLDER = "test-output/";
-    private static final String FILE_NAME = "测试报告"+System.currentTimeMillis()+".html";
+    private static final String FILE_NAME = "index"+".html";
 
     private ExtentReports extent;
 
